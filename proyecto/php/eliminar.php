1330ee8f642432e040ec826a4541aa62742fa316
martinWeissmann <127246720+martinWeissmann@users.noreply.github.com>
2025-01-23 17:28:17 -0300

    XZZ

--- proyecto/php/eliminar.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'partido.php';
-
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    eliminarPartido($id);
-    header('Location: partidos.html'); // Redirige después de la eliminación
-}
-?>
--- proyecto/scripts/supabaseClient.js
+++ proyecto/scripts/supabaseClient.js
@@ -1,46 +1,6 @@
 // Conexión con Supabase
 const supabaseUrl = 'https://imokfxicqzmedegxskxp.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imltb2tmeGljcXptZWRlZ3hz' ;
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imltb2tmeGljcXptZWRlZ3hza3hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczMTY0MTMsImV4cCI6MjA1Mjg5MjQxM30.Hu763dw72zipJztCLyKy66fuwoW0_GTaLkw8PwKb_8s' ;
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
-// Manejar el formulario de inicio de sesión
-const loginForm = document.getElementById('login-form');
-loginForm.addEventListener('submit', async (e) => {
-  e.preventDefault();
   
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
-
-  if (error) {
-    alert('Error: ' + error.message);
-  } else {
-    alert('¡Inicio de sesión exitoso!');
-    console.log(data);
-  }
-});
-
-// Manejar el registro de usuario
-const signupLink = document.getElementById('signup-link');
-signupLink.addEventListener('click', async (e) => {
-  e.preventDefault();
-
-  const email = prompt('Introduce tu correo electrónico:');
-  const password = prompt('Introduce tu contraseña:');
-
-  const { data, error } = await supabase.auth.signUp({
-    email,
-    password,
-  });
-
-  if (error) {
-    alert('Error: ' + error.message);
-  } else {
-    alert('¡Registro exitoso! Revisa tu correo para verificar tu cuenta.');
-    console.log(data);
-  }
-});
