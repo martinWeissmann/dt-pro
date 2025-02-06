0163271c4717f57cfd1248f98dcadfe7ee2dd1d1
martinWeissmann <127246720+martinWeissmann@users.noreply.github.com>
2025-02-06 14:16:20 -0300

    TERMINADO

--- proyecto/scripts/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-// Conexión con Supabase
-const supabaseUrl = 'https://zeznffvtftfpyintwvjf.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inplem5mZnZ0ZnRmcHlpbnR3dmpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxMDg4MzEsImV4cCI6MjA1MzY4NDgzMX0.HAHAJ5N7K5O5X-plI_o8eb3uMV7Yicx7O9k4WwStuLw' ;
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
-
-  
